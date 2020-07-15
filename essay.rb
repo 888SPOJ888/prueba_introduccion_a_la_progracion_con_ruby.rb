c993e33c66ea9ce07f372f77243c8473dc5567e3
alexis Montealegre <ajmu26@gmail.com>
2020-07-15 14:05:31 -0400

    buildind api_key

--- essay.rb
+++ essay.rb
@@ -9,22 +9,49 @@ def request(url_requested)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(url)
-    request["app_id"] = ''
-    request["app_key"] = '' 
+    request["app_id"] = '3cb74d10-8031-43c4-afa9-c3b4393ff4fa '
+    request["app_key"] = 'ZJ2ZBVN4z4BhxoNPmILVaAh12jMiyCGBr8gMlPEw' 
     response = http.request(request)
     return JSON.parse(response.body)
 end
 
-data = request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=ZJ2ZBVN4z4BhxoNPmILVaAh12jMiyCGBr8gMlPEw') 
+body = request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=ZJ2ZBVN4z4BhxoNPmILVaAh12jMiyCGBr8gMlPEw') 
 
+new_array = body["photos"]
+values = (new_array.map {|photos| [photos["img_src"]]})
+values2 = (new_array.map {|photos| [photos["camera"]]})
 
-def create_web_page(array)
-    output = "<html>\n/<head>\n</head>\n<body>\n<ul>"
+def build_web_page(array)
+    output = "\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<ul>\n"
     array.each do |photo|
-        output += "\t<li><img src=\"#{photo}\"></li>\n"
+        photo.each do |ele|
+            output += "\t\t\t<li><img src=#{ele} /> </li>\n"
+        end
     end
-    output += "</ul>\n</body>\n</html>"
+    output += "\t\t</ul>\n\t</body>\n</html>"
     File.write('curiosity.html', output)
     
 end
-create_web_page(data)
+build_web_page(values)
+
+def photos_count(count)
+    new_hash = {}
+    final_array = []
+    count.each do |camera|
+        camera.each do |ele|
+            ele.each do |k, v|
+                if k == "full_name"
+                    new_hash[k] = v
+                    final_array += new_hash.values
+                end
+            end
+        end
+    end
+    grouped_camera = final_array.group_by {|x| x}
+    grouped_camera.each do |k, v|
+        grouped_camera[k] = v.count
+    end
+    print grouped_camera
+end
+
+photos_count(values2)
--- first_request.rb
+++ first_request.rb
@@ -1,20 +1,58 @@
-require "uri"
-require "net/http"
+
+require 'uri' 
+require 'net/http'
+require 'openssl'
 require 'json'
 
-def request(address)
-    url = URI(address)
-    url = URI("https://jsonplaceholder.typicode.com/posts")
-    https = Net::HTTP.new(url.host, url.port);
-    https.use_ssl = true
+def request(url_requested) 
+    url = URI(url_requested)
+    http = Net::HTTP.new(url.host, url.port) 
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(url)
-    request["Cookie"] = "__cfduid=df87f96a76cb2da0fcb5c3914fb71a3191594607495"
-
-    response = https.request(request)
-    JSON.parse response.read_body
-    end
-    
-    body = request("https://jsonplaceholder.typicode.com/posts")
-    body.each do |post|
-    puts post['title']
+    request["app_id"] = '3cb74d10-8031-43c4-afa9-c3b4393ff4fa '
+    request["app_key"] = 'ZJ2ZBVN4z4BhxoNPmILVaAh12jMiyCGBr8gMlPEw' 
+    response = http.request(request)
+    return JSON.parse(response.body)
 end
+
+body = request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=ZJ2ZBVN4z4BhxoNPmILVaAh12jMiyCGBr8gMlPEw') 
+
+new_array = body["photos"]
+values = (new_array.map {|photos| [photos["img_src"]]})
+values2 = (new_array.map {|photos| [photos["camera"]]})
+
+def build_web_page(array)
+    output = "\n<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<ul>\n"
+    array.each do |photo|
+        photo.each do |ele|
+            output += "\t\t\t<li><img src=#{ele} /> </li>\n"
+        end
+    end
+    output += "\t\t</ul>\n\t</body>\n</html>"
+    File.write('curiosity.html', output)
+    
+end
+build_web_page(values)
+
+def photos_count(count)
+    new_hash = {}
+    final_array = []
+    count.each do |camera|
+        camera.each do |ele|
+            ele.each do |k, v|
+                if k == "full_name"
+                    new_hash[k] = v
+                    final_array += new_hash.values
+                end
+            end
+        end
+    end
+    grouped_camera = final_array.group_by {|x| x}
+    grouped_camera.each do |k, v|
+        grouped_camera[k] = v.count
+    end
+    print grouped_camera
+end
+
+photos_count(values2)
